24f0dae04f540e1800839452cde2747b45108d83
BroKoda <kottix96@gmail.com>
2022-08-04 19:10:53 +0200

    github pages deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+// GITHUB PAGES DEPLOYMENT
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/lottery-mito-test/'
+      : '/'
+}
