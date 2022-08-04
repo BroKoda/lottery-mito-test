4a8e7925ef8dbc0777f1dcd7902e45f53a3c6e03
BroKoda <kottix96@gmail.com>
2022-08-04 18:53:29 +0200

    Hello World - project init

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './assets/styles/main.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +25,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
