6eabb71f23984a037ca621230928af408745881a
Konrad Kavai <kottix96@gmail.com>
2022-08-05 13:15:15 +0200

    build mobil optimalizacio

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
       </div>
     </div>
     <div class="row justify-content-start">
-      <div class="col-6">
+      <div class="col-12 col-md-6 pe-md-0">
         <div class="container ticket-counter p-3">
           <div class="row">
             <div class="col-8">
@@ -63,19 +63,19 @@ export default {
       <div class="col-12 my-3">
         <div class="container px-0 match-counter-container">
           <div class="row match-counter-row mx-0">
-            <div class="col-3 match-counter-box align-self-center text-center">
+            <div class="col-6 col-md-3 match-counter-box align-self-center text-center">
               <p>2 talalat</p>
               <p class="js-two-matches">0</p>
             </div>
-            <div class="col-3 match-counter-box align-self-center text-center">
+            <div class="col-6 col-md-3 match-counter-box align-self-center text-center">
               <p>3 talalat</p>
               <p class="js-three-matches">0</p>
             </div>
-            <div class="col-3 match-counter-box align-self-center text-center">
+            <div class="col-6 col-md-3 match-counter-box align-self-center text-center">
               <p>4 talalat</p>
               <p class="js-four-matches">0</p>
             </div>
-            <div class="col-3 match-counter-box align-self-center text-center">
+            <div class="col-6 col-md-3 match-counter-box align-self-center text-center">
               <p>5 talalat</p>
               <p class="js-five-matches">0</p>
             </div>
@@ -85,11 +85,11 @@ export default {
     </div>
 
     <div class="row">
-      <div class="col-3 align-self-center my-2">
+      <div class="col-12 col-md-3 align-self-center my-2 text-center text-md-start">
         <p class="winning-numbers-title">Winning numbers:</p>
       </div>
-      <div class="col-9 my-2">
-        <div class="winning-number-container align-items-center flex-row d-flex js-winning-numbers-container">
+      <div class="col-12 col-md-9 my-2">
+        <div class="winning-number-container align-items-center flex-row d-flex js-winning-numbers-container justify-content-center justify-content-md-start">
           <p class="winning-number-box js-winning-number"></p>
           <p class="winning-number-box js-winning-number"></p>
           <p class="winning-number-box js-winning-number"></p>
@@ -100,11 +100,11 @@ export default {
     </div>
 
     <div class="row">
-      <div class="col-3 align-self-center my-2">
+      <div class="col-12 col-md-3 align-self-center my-2 text-center text-md-start">
         <p class="player-number-title">Player numbers:</p>
       </div>
-      <div class="col-9 my-2">
-        <div class="player-number-container align-items-center flex-row d-flex">
+      <div class="col-12 col-md-9 my-2">
+        <div class="player-number-container align-items-center flex-row d-flex justify-content-center justify-content-md-start">
           <input type="number" min="1" max="90" class="player-number-box js-player-number"/>
           <input type="number" class="player-number-box js-player-number"/>
           <input type="number" class="player-number-box js-player-number"/>
@@ -115,10 +115,10 @@ export default {
     </div>
 
     <div class="row">
-      <div class="col-3 align-self-center my-2">
+      <div class="col-12 col-md-3 text-center text-md-start align-self-center my-2">
         <p class="speed-title">Speed:</p>
       </div>
-      <div class="col-9 align-self-center my-2">
+      <div class="col-12 col-md-9 text-center text-md-start align-self-center my-2">
         <div class="speed-container">
           <input type="range" min="1" max="1000" value="200" class="speed">
         </div>
@@ -128,13 +128,13 @@ export default {
     <div class="row">
       <div class="col-12 lottery-controls mt-5">
         <div class="row">
-          <div class="col-4">
+          <div class="col-12 col-sm-6 col-md-4 mb-3 mb-md-0">
             <a href="#" type="button" class="lottery-button js-get-player-number">Start</a>
           </div>
-          <div class="col-4">
+          <div class="col-12 col-sm-6 col-md-4 mb-3 mb-md-0">
             <a href="#" type="button" class="lottery-button js-stop">Stop</a>
           </div>
-          <div class="col-4">
+          <div class="col-12 col-md-4">
             <a href="#" type="button" class="lottery-button js-random-numbers">Random numbers</a>
           </div>
         </div>
